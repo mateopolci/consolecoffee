2d4b6d8be90daa6304e35dc558e8eaceb1bdcc63
mateopolci <mateopolci@hotmail.com>
2024-08-01 19:39:50 -0300

    Final

--- src/js/main.js
+++ src/js/main.js
@@ -27,14 +27,14 @@ const list = () => {
     alert(`Coffee list: \n\n${message}`);
 }
 
-//2 & 3. List coffees by price
+//2 & 3. List coffees by price.
 const orderedList = (order) => { 
     const orderedList = [...coffeeList];
-    if (order === "asc") {
+    if(order === "asc"){
         orderedList.sort((a,b) => a.price - b.price);
         const orderedMessage = orderedList.map(el => `-${el.name}, ${el.origin}, USD$${el.price}`).join('\n\n');
         alert(`Coffee list ordered by ascendent price: \n\n${orderedMessage}`);
-    } else if (order === "desc"){
+    }else if(order === "desc"){
         orderedList.sort((a,b) => b.price - a.price);
         const orderedMessage = orderedList.map(el => `-${el.name}, ${el.origin}, USD$${el.price}`).join('\n\n');
         alert(`Coffee list ordered by descendent price: \n\n${orderedMessage}`);
@@ -71,46 +71,44 @@ const addCart = () => {
         }
         const searched = coffeeList.find(el => el.id === item);
         cart.push(searched);
-        loop = confirm("Would you like to add another item to the cart?")
-    }while (loop === true)
+        loop = confirm("Would you like to add another item to the cart?");
+    }while (loop === true);
 }
 
 //6. Remove coffees from cart.
 const removeCart = () => {
     let loop;
     do{
-        if (cart.length === 0) {
+        if(cart.length === 0){
             alert("The cart is empty.");
             return;
         }
         const message = cart.map((el) => `${el.id} ${el.name}, ${el.origin}, USD$${el.price}`).join('\n\n');
         let item = parseInt(prompt(`Select one item to remove from the cart:\n\n${message}`));
-        if (item <= 0 || item > coffeeList.length){
+        if(item <= 0 || item > coffeeList.length){
             alert("Warning, the item must exist.");
             return;
         }
-        const removedItem = cart.find(el => el.name === item);
         const newCart = cart.filter(el => el.id !== item);
         cart = newCart;
-        loop = confirm(`The item ${removedItem} has been removed from the cart. Would you like to remove another one?`);
-    }while (loop === true)
+        loop = confirm(`The item has been removed from the cart. Would you like to remove another one?`);
+    }while(loop === true);
 }
 
 //7. Show cart.
 const showCart = () => {
-    if (cart.length === 0) {
+    if(cart.length === 0){
         alert("The cart is empty.");
         return;
     }
     const message = cart.map(el => `-${el.name}, ${el.origin}, USD$${el.price}`).join('\n\n');
     const total = parseFloat(cart.reduce((sum, item) => sum + item.price, 0).toFixed(2));
-    //const total = cart.reduce((sum, item) => sum + item.price, 0);
     alert(`Current cart total: USD$${total}\n\nCurrent cart:\n\n${message}`);
 }
 
 //8. Checkout.
 const checkout = () => {
-    if (cart.length === 0) {
+    if(cart.length === 0){
         alert("The cart is empty.");
         return;
     }
@@ -118,36 +116,36 @@ const checkout = () => {
     const total = cart.reduce((sum, item) => sum + item.price, 0);
     if(confirm(`Your total is USD$${total}. Proceed to Checkout?`)){
         let cardOwner = prompt("Enter your name as it appears on your credit/debit card.");
-        if (cardOwner === null || cardOwner === "" || cardOwner.toString().length < 5 || cardOwner.toString().length > 40) {
+        if(cardOwner === null || cardOwner === "" || cardOwner.toString().length < 5 || cardOwner.toString().length > 40){
             alert("Warning! The name appears to be empty, too short or too long.");
             return;
         }
         let cardNumber = parseInt(prompt("Enter your card number."));
         let cardNumberString = cardNumber.toString();
-        if (cardNumber === NaN ||cardNumber === 0 || cardNumber === null || cardNumberString.length < 14 || cardNumberString.length > 16) {
+        if(cardNumber === NaN ||cardNumber === 0 || cardNumber === null || cardNumberString.length < 14 || cardNumberString.length > 16){
             alert("Warning! The number should range from 14 to 16 digits.");
             return;
         }
         let cardCode = parseInt(prompt("Enter your card security code."));
         let cardCodeString = cardCode.toString();
-        if (cardNumber === NaN ||cardCode === 0 || cardCode === null || cardCodeString.length < 3 || cardCodeString.length > 4) {
+        if(cardNumber === NaN ||cardCode === 0 || cardCode === null || cardCodeString.length < 3 || cardCodeString.length > 4){
             alert("Warning! The code should range from 3 to 4 digits.");
             return;
         }
         let cardExpiryDate = prompt("Enter your card expiry date (MM/YYYY).");
-        if (cardExpiryDate === null || cardExpiryDate === "") {
+        if(cardExpiryDate === null || cardExpiryDate === ""){
             alert("Warning! You must enter a valid card expiry date.");
             return;
         }
         alert(`Thank you for your purchase at Console Commerce!\nHere's your recipt:\n\nFull name: ${cardOwner}\nPurchase date: ${new Date().toLocaleString()}\nItems bought:\n\n${message}\n\nTotal: USD$${total}`);
     }
 }
-
+// Menu.
 const menu = () => {
     let loop;
     do{  
         let op = parseInt(prompt(`Welcome to Console Coffee☕! Enter a number to pick an option:\n\n1. List all coffees.\n2. List all coffees by ascendent price.\n3. List all coffees by descendent price.\n4. List coffee by specific origin.\n5. Add coffes to cart.\n6. Remove coffees from cart.\n7. Show cart.\n8. Checkout.`));
-        switch (op) {
+        switch (op){
             case 1:
                 //1. List all coffees by ascendent price.
                 list();
